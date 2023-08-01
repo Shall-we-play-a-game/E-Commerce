28524c0180781c33a6138e2f148ed74ab2d003c2
Ibrahim Khan <92690585+Shall-we-play-a-game@users.noreply.github.com>
2023-08-01 14:45:53 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -37,8 +37,9 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-app.use("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "client/build/index.html"));
+app.get("*", function (req, res) {
+  const index = path.join(__dirname, 'client', 'build', 'index.html');
+  res.sendFile(index)
 });
 
 
